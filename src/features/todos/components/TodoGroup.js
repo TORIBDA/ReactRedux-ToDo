ea6ecfc039b93465897871ccc3815818cbbf089b
TORIBDA <david.moses.toribio@oocl.com>
2021-08-20 01:50:27 +0800

    [TodoList][TORIBDA] refactor: removed the setting of the todos on the TodoGroup

--- src/features/todos/components/TodoGroup.js
+++ src/features/todos/components/TodoGroup.js
@@ -1,18 +1,9 @@
 import TodoItem from "./TodoItem";
-import { selectTodoIds, AddManyTodo} from "../reducers/todosSlice";
-import { useSelector, useDispatch,  } from "react-redux";
-import { getAllTodos } from "../../../api/todosAPI"
-import {useEffect} from "react";
+import { selectTodoIds } from "../reducers/todosSlice";
+import { useSelector } from "react-redux";
 
 function TodoGroup() {
     const todoIds = useSelector(selectTodoIds);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        getAllTodos().then((response) => {
-            dispatch(AddManyTodo(response.data))
-        })
-    })
 
     return (
         <div>
