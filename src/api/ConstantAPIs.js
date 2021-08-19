ad70ba74d15d37a595fe731d764eedc890ed2864
TORIBDA <david.moses.toribio@oocl.com>
2021-08-20 01:47:26 +0800

    [TodoList][TORIBDA] refactor: changed baseURL

--- src/api/ConstantAPIs.js
+++ src/api/ConstantAPIs.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const ConstantAPIs = axios.create({
-    baseURL: "https://611cc220a18e850017decc45.mockapi.io/"
+    baseURL: "http://localhost:8090/"
 });
 
 export default ConstantAPIs;
+
+//baseURL: "http://localhost:8090/"
+//baseURL: "https://611cc220a18e850017decc45.mockapi.io/"
