8c20da0bbfbe53c1b7bd10384c01d639dce39ea5
TORIBDA <david.moses.toribio@oocl.com>
2021-08-19 10:03:39 +0800

    [Todo][TORIBDA] refactor: changed the document title

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import './App.css';
 import TodoList from './features/todos/components/TodoList';
 import TodoDoneList from './features/todos/components/TodoDoneList';
 import {Route, Link, BrowserRouter, Switch} from "react-router-dom";
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout, { Header } from 'antd/lib/layout/layout';
 import { Menu } from 'antd';
 
 function App() {
+  useEffect(() => {
+    document.title = "To Do List";
+  }, []);
+
   return (
     <React.Fragment>
       <BrowserRouter>
         <Layout>
           <Header>
             <Menu theme="dark"  mode="horizontal">
-              <Menu.Item>
+              <Menu.Item key="1">
                 <Link to="/">Home</Link>
               </Menu.Item>
-              <Menu.Item>
+              <Menu.Item key="2">
                 <Link to="/todos">Todo List</Link>
               </Menu.Item>
-              <Menu.Item>
+              <Menu.Item key="3">
                 <Link to="/done">Done list</Link>
               </Menu.Item>
             </Menu>
